3cc4c1f590937603b63e8a756929de5027896a4d
Thomas Dahlgren <dahlg136@morris.umn.edu>
2022-02-23 12:50:38 -0600

    Start lab 6

---
+++ Assets/Scripts/FPS/FPSUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSUI : MonoBehaviour
+{
+    [SerializeField] private Image healthBar;
+    [SerializeField] private TMP_Text enemyDefeatText;
+    public void ShowHealthFraction(float fraction)
+    {
+        healthBar.fillAmount = fraction;
+    }
+
+    public void ShowEnemyDefeatCount(int count)
+    {
+        enemyDefeatText.text = "Enemies Defeated: " + count;
+    }
+}
